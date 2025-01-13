92d657eb7724c1d4cbf2be4584a9681737dce360
Lukas Voegtle <5764745+voegtlel@users.noreply.github.com>
2025-01-13 20:19:21 +0100

    Fix sendapi

--- src/scanapp/widgets/sendapi.py
+++ src/scanapp/widgets/sendapi.py
@@ -15,13 +15,19 @@ class ApiSender(QThread):
 
     def __init__(self, parent, name: str, purpose: str, iban: str, attachments: list[Attachment]):
         super().__init__(parent)
+        self.json_data = {
+            'name': name,
+            'purpose': purpose,
+            'iban': iban,
+        }
+        self.attachments = attachments
         self.files = (
             ("name", (None, name)),
             ("purpose", (None, purpose)),
             ("iban", (None, iban)),
             *(
                 (attachment.name, (attachment.name, attachment.data, f"{attachment.mime_main}/{attachment.mime_sub}"))
-                for attachment in self.attachments
+                for attachment in attachments
             )
         )
 
@@ -38,7 +44,7 @@ class ApiSender(QThread):
             os.makedirs("failed_data", exist_ok=True)
             filename_base = f"failed_data/{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
             with open(f"{filename_base}.json", 'w') as wf:
-                json.dump(self.json_data, wf)
+                json.dump(self.json_data, wf, indent=2)
             for attachment in self.attachments:
                 with open(f"{filename_base}.{attachment.name}", 'wb') as wbf:
                     wbf.write(attachment.data)
